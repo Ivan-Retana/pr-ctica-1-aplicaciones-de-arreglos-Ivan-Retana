0753442b5b285f765db2be64a84792b60f65c890
Ivan-Retana <ianyretans@gmail.com>
2025-02-22 14:34:40 -0600

    Create Pr01E01.c

---
+++ Pr01E01.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+double main() {
+
+    double arr[5] = {0, 1, 2, 3, 4};
+    
+    for (double i = 0; i < 5; i++) {
+        printf(" %p\n", &arr[5]);
+    }
+    
+   
+    return 0;
+}
